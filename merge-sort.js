a4a781a0ba8db460125c494937fb79a4a7371b1f
heroes75 <123456789+odin@users.noreply.github.com>
2025-05-08 13:59:55 +0000

    finish all assignement

--- merge-sort.js
+++ merge-sort.js
@@ -2,17 +2,14 @@ function merge(arr) {
     console.log("here start the recursion");
     
     const isSort = (arr) => arr.every((el, i, array) =>  el <= array[i + 1 >= array.length ? i : i + 1]) && arr.slice(arr.length / 2).every((el, i, array) =>  el <= array[i + 1 >= array.length ? i : i + 1])
-    /*if(arr.length === 2 && !arr.every((el, i ,array) => el <= array[i + 1 >= array.length ? i : i + 1])) {
-        if (arr[0] < arr[1]) return arr;
-        else return arr.reverse();
-    }*/
-    console.log("2nd if",arr.slice(0, arr.length / 2).every((el, i, array) =>  el <= array[i + 1 >= array.length ? i : i + 1]) && arr.slice(arr.length / 2).every((el, i, array) =>  el <= array[i + 1 >= array.length ? i : i + 1]));
-    //if(isSort(arr)) return
-    if(arr.slice(0, arr.length / 2).every((el, i, array) =>  el <= array[i + 1 >= array.length ? i : i + 1]) && arr.slice(arr.length / 2).every((el, i, array) =>  el <= array[i + 1 >= array.length ? i : i + 1])) {
+    
+    if(isSort(arr.slice(0, arr.length / 2)) && isSort(arr.slice(arr.length / 2))) {
         const left = arr.slice(0, arr.length / 2);
         const right = arr.slice(arr.length / 2);
         let newArr = [];
-        console.log(arr)
+        console.log(arr);
+        console.log(left);
+        console.log(right);
         for (let i = 0; i <= arr.length; i++) {
             if(left.length === 0) {
                 newArr = newArr.concat(right)
@@ -24,12 +21,12 @@ function merge(arr) {
                 console.log("r =0", newArr)
                 break
             }
-            if(left[i] < right[i]) {
-                newArr.push(left[i]);
+            if(left[0] < right[0]) {
+                newArr.push(left[0]);
                 left.shift()
                 console.log("l <r", newArr)
             } else {
-                newArr.push(right[i])
+                newArr.push(right[0])
                 right.shift()
                 console.log("l > g", newArr)
             }
@@ -38,25 +35,27 @@ function merge(arr) {
         console.log(newArr)
         return newArr
     }
-    //console.log(merge(arr.slice(0, arr.length / 2)))
-    //while (!arr.every((el, i ,array) => el <= array[i + 1 >= array.length ? i : i + 1])) {
-    console.log("in while", arr);
-    //console.log("in while", !arr.every((el, i ,array) => el <= array[i + 1 >= array.length ? i : i + 1]));
+
+    while (!isSort(arr)) {
         
-        arr = merge(arr.slice(0, arr.length / 2)).concat(merge(arr.slice(arr.length / 2)))
-    //}
+        arr = merge(arr.slice(0, arr.length / 2)).concat(merge(arr.slice(arr.length / 2)));
+        arr = merge(arr)
+    }
     console.log("after while",arr)
     return arr
 }
 
-console.log(merge([3, 2, 1, 13, 8, 5, 0, 1]));
-console.log([
-  2, 3, 1, 13,
-  5, 8, 0,  1
-].every((el, i, array) =>  {
-    //console.log(el <= array[i + 1 >= array.length ? i : i + 1]);
-    //console.log(i);
-    
-    return el <= array[i + 1 >= array.length ? i : i + 1]
-}));
+
+const generateArray = () => {
+    const array = []
+    for (let i = 0; i < 111; i++) {
+        array.push(Math.floor(Math.random() * 10000))
+    }
+    return array
+}
+const arrayOf100 = generateArray()
+
+console.log(merge(arrayOf100));
+console.log(arrayOf100);
+
 
